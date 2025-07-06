51eaf58e5e24a7d2963bff15d5d51f7add2d7b14
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-06 13:39:45 -0300

    Feat: adding enums

---
+++ src/main/kotlin/com/miniflix/enums/MovieGenreEnum.kt
@@ -0,0 +1,22 @@
+package com.miniflix.enums
+
+enum class MovieGenreEnum {
+    ACTION,
+    ANIMATION,
+    ADVENTURE,
+    ANIME,
+    CLASSIC,
+    COMEDY,
+    DOCUMENTARY,
+    DRAMA,
+    FANTASY,
+    HORROR,
+    INTERNATIONAL,
+    KIDS,
+    MUSICAL,
+    POLICE,
+    ROMANCE,
+    SLASHER,
+    SPORTS,
+    THRILLER
+}
